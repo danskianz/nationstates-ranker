7328cc7350842ccc2537a9a1289f3aae802e8398
danie <danie@DESKTOP-CS7J07M.attlocal.net>
2017-11-24 22:44:44 -0600

    Add default number of fetched top and bottom nations Remove unused code in scoring a nation

--- ranker/src/main/java/com/danskianz/nationstates/ranker/Leaderboard.java
+++ ranker/src/main/java/com/danskianz/nationstates/ranker/Leaderboard.java
@@ -28,6 +28,8 @@ import javax.ws.rs.core.UriInfo;
 @ManagedBean
 @Path("leaderboard")
 public class Leaderboard {
+    
+    private static final int DEFAULT_NUM_OF_NATIONS = 5;
 
     @Inject
     private RegionService provider;
@@ -35,6 +37,14 @@ public class Leaderboard {
     @Context
     private UriInfo allUri;
 
+    /**
+     * Get Top X Nations for a Region
+     * 
+     * @param region
+     * @return the top n ranking nations in a region.
+     * If no query parameter is specified, it returns a default of 5 top
+     * nations in the list.
+     */
     @GET
     @Path("regions/{region}/elites")
     @Produces(MediaType.APPLICATION_JSON)
@@ -47,13 +57,24 @@ public class Leaderboard {
                 .getRegionCensus(region);
 
         Map<String, Double> standings = getStandings(regionCensus);
-
+        
+        int nations = params.isEmpty() ? DEFAULT_NUM_OF_NATIONS :
+                Integer.parseInt(params.getFirst("top"));
+        
         return standings.entrySet().stream()
                 .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                .limit(Integer.parseInt(params.getFirst("top")))
+                .limit(nations)
                 .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
     }
     
+    /**
+     * Get Bottom X Nations for a Region
+     * 
+     * @param region
+     * @return the bottom n ranking nations in a region.
+     * If no query parameter is specified, it returns a default of 5 bottom
+     * nations in the list.
+     */
     @GET
     @Path("regions/{region}/unspeakables")
     @Produces(MediaType.APPLICATION_JSON)
@@ -61,6 +82,9 @@ public class Leaderboard {
             @PathParam("region") String region) {
 
         MultivaluedMap<String, String> params = allUri.getQueryParameters();
+        
+        int nations = params.isEmpty() ? DEFAULT_NUM_OF_NATIONS :
+                Integer.parseInt(params.getFirst("bottom"));
 
         Map<String, List<CensusScore>> regionCensus = provider
                 .getRegionCensus(region);
@@ -69,7 +93,7 @@ public class Leaderboard {
 
         return standings.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue())
-                .limit(Integer.parseInt(params.getFirst("bottom")))
+                .limit(nations)
                 .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
     }
 
@@ -85,14 +109,18 @@ public class Leaderboard {
         return standings;
     }
     
+    /**
+     * Get the Raw Score for a Nation
+     * 
+     * @param nation
+     * @return the score of the specified nation
+     */
     @GET
     @Path("nations/{nation}")
     @Produces(MediaType.APPLICATION_JSON)
     public Map<String, Double> getNationScore(
             @PathParam("nation") String nation) {
-
-        MultivaluedMap<String, String> params = allUri.getQueryParameters();
-
+        
         List<CensusScore> nationCensus = provider.getNationCensus(nation);
         
         return Collections.singletonMap(nation, Ranker.rawScore(nationCensus));
