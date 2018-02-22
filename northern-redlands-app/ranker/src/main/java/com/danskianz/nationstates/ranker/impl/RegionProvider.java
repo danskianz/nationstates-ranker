09b42e8023f594f62a945e74bc62508dd1f50ab4
danie <danie@DESKTOP-CS7J07M.attlocal.net>
2018-02-22 01:15:34 -0600

    Add safeguard against spamming the NationStates API

--- northern-redlands-app/ranker/src/main/java/com/danskianz/nationstates/ranker/impl/RegionProvider.java
+++ northern-redlands-app/ranker/src/main/java/com/danskianz/nationstates/ranker/impl/RegionProvider.java
@@ -8,12 +8,14 @@ import com.github.agadar.nationstates.domain.region.Region;
 import com.github.agadar.nationstates.enumerator.CensusId;
 import com.github.agadar.nationstates.shard.NationShard;
 import com.github.agadar.nationstates.shard.RegionShard;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 /**
  * Uses Agadar's NationStates Interface.
@@ -24,35 +26,45 @@ public class RegionProvider {
     
     private final Map<String, List<String>> regionNationMap;
 
+    // limit requests to be processed by a daemon thread
+    private final ExecutorService censusPool = Executors.newFixedThreadPool(2);
+
     public RegionProvider() {
         NationStates.setUserAgent(RankerConstants.APP_USER_AGENT);
         regionNationMap = new ConcurrentHashMap<>();
     }
 
+    /**
+     *
+     * @param region
+     * @return
+     */
     public Map<String, List<CensusScore>> getRegionCensus(String region) {
 
-        Map<String, List<CensusScore>> regionCensus = new HashMap<>();
+        CompletableFuture<Region> promisedLand = CompletableFuture.supplyAsync(
+                NationStates.region(region)
+                        .shards(RegionShard.NATION_NAMES)::execute, censusPool);
 
-        List<String> nationNames = new ArrayList<>();
-        
-        Region land = NationStates.region(region)
-                .shards(RegionShard.NATION_NAMES).execute();
-        
-        land.nationNames.forEach((nation) -> {
-            regionCensus.put(nation, getNationCensus(nation));
-            nationNames.add(nation);
-        });
-        
-        regionNationMap.put(region, nationNames);
+        Region land = promisedLand.join();
+
+        Map<String, List<CensusScore>> regionCensus = land.nationNames
+                .stream().collect(Collectors.toMap(
+                        nation -> nation,
+                        nation -> getNationCensus(nation)));
+
+        regionNationMap.put(region, land.nationNames);
 
         return regionCensus;
     }
 
     public List<CensusScore> getNationCensus(String nation) {
 
-        Nation country = NationStates.nation(nation)
-                .censusIds(CensusId.values()).shards(NationShard.CENSUS)
-                .execute();
+        CompletableFuture<Nation> promisedCountry = CompletableFuture
+                .supplyAsync(NationStates.nation(nation)
+                        .censusIds(CensusId.values())
+                        .shards(NationShard.CENSUS)::execute, censusPool);
+
+        Nation country = promisedCountry.join();
 
         return (country != null)
                 ? country.census : Collections.<CensusScore>emptyList();
